0a3ea8725c1da80ec6e57951adf1f38c8645177e
Talha Hasan Zia <talhahasanzia@hotmail.com>
2018-01-08 16:45:51 +0500

    working example of encryption with a key

---
+++ SQLcipherDemo/app/src/main/java/com/playground/sqlcipherdemo/DbHelper.java
@@ -0,0 +1,12 @@
+package com.playground.sqlcipherdemo;
+
+import net.sqlcipher.database.SQLiteOpenHelper;
+
+/**
+ * Created by Talha Hasan Zia on 08-Jan-18.
+ * <p></p><b>Description:</b><p></p> Why class was created?
+ * <p></p>
+ * <b>Public Methods:</b><p></p> Only listing to public methods usage.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+}
---
+++ SQLcipherDemo/app/src/main/java/com/playground/sqlcipherdemo/DbUtil.java
@@ -0,0 +1,21 @@
+package com.playground.sqlcipherdemo;
+
+import android.content.Context;
+
+/**
+ * Created by Talha Hasan Zia on 08-Jan-18.
+ * <p></p><b>Description:</b><p></p> Utility class for Database dummy operations.
+ * <p></p>
+ * <b>Public Methods:</b><p></p> Only listing to public methods usage.
+ */
+public class DbUtil
+{
+
+    private Context context;
+
+    public DbUtil(Context context) {
+        this.context = context;
+    }
+
+
+}
